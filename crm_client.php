1040e927c15f618e57233a955a59722107bf3f06
jamil Bulatov <jamil.bulatov@gmail.com>
2017-04-07 21:46:35 +0300

    вывод ошибки + установка параметров

--- crm_client.php
+++ crm_client.php
@@ -3,26 +3,38 @@ require_once($_SERVER["DOCUMENT_ROOT"]."/curl_client.php");
 
 class CrmClient {
 
+  static $PORTAL_URL;
+  static $RESPONSIBLE;
+  static $LOGIN;
+  static $PASSWORD;
+  static $LAST_ERROR;
+
+  const PATH_REST_API = '/crm/configs/import/lead.php';
+ 
   private function __construct() {}
 
   public static function sendToCRM(array $params) {
 
-  if(!array_key_exists('TITLE',$params) || !array_key_exists('EMAIL_HOME',$params) 
-     || !array_key_exists('NAME',$params)) {
+  if(!array_key_exists('TITLE',$params) 
+    || !array_key_exists('EMAIL_HOME',$params) 
+    || !array_key_exists('NAME',$params)) {
 
-     return false;
+    return false;
   }
 
   $default_params = array(
-    "LOGIN" => "admin",
-    "PASSWORD" => "212121",
+    "LOGIN"     => CrmClient::$LOGIN,
+    "PASSWORD"  => CrmClient::$PASSWORD,
     "SOURCE_ID" => "WEB_FORM",
     "STATUS_ID" => "NEW",
-    "ASSIGNED_BY_ID" => 1,
+    "ASSIGNED_BY_ID" => CrmClient::$RESPONSIBLE,
   );
+
   $params = array_merge($default_params,$params);
 
-  $http_client = new CurlClient("http://b24.internetdeveloper.ru/crm/configs/import/lead.php");
+  $url = self::$PORTAL_URL.CrmClient::PATH_REST_API;
+
+  $http_client = new CurlClient($url);
   $http_client->setParams($params);
   $responce = $http_client->send(true);
 
@@ -30,6 +42,8 @@ class CrmClient {
 
       return true;
   }
+
+  self::$LAST_ERROR = $responce['error'];
 
   return false;
  }
